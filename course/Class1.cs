9cc1813906a904d138cebbdf05e8682fb5f4856f
Evgeny Lyzov <lyzov.e.r@gmail.com>
2016-04-29 20:21:18 +0600

    Init commit. Implemented theta estimation

---
+++ course/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
+
+namespace course
+{
+    class DFModel
+    {
+        Vector<double> theta_ex;
+        Vector<double> theta;
+        Vector<double> y;
+        Matrix<double> X;
+        int k1, k2;
+            
+        public DFModel(int k1, int k2, double[] tet, double[][] X, double ro)
+        {
+            this.k1 = k1;
+            this.k2 = k2;
+            this.theta_ex = DenseVector.OfArray(tet);
+            this.X = DenseMatrix.OfRowArrays(X);
+            generate(ro);
+        }
+
+        public double[] tet()
+        {
+            return theta.ToArray();
+        }
+
+        private void generate(double ro)
+        {
+            var n = X.RowCount;
+            var u = X * theta_ex;
+            var u_mean = u - Statistics.Mean(u);
+            var w2 = (u_mean * u_mean) / (n - 1);
+            Vector<double> err = DenseVector.OfArray(Generate.Normal(n, 0, ro * w2));
+            y = u + err;
+        }
+
+        public void estimate()
+        {
+            est_theta();
+        }
+
+        private void est_theta()
+        {
+            var l1 = new List<double>();
+            l1.Add(0);
+            l1.AddRange(Enumerable.Repeat(1.0, k1));
+            l1.AddRange(Enumerable.Repeat(0.0, k2));
+            var l2 = new List<double>();
+            l2.Add(0);
+            l2.AddRange(Enumerable.Repeat(0.0, k1));
+            l2.AddRange(Enumerable.Repeat(1.0, k2));
+            Matrix<double> U = DenseMatrix.OfRowArrays(new double[][]{ l1.ToArray(), l2.ToArray() });
+            var u = U * theta_ex;
+            var r = (X.Transpose() * y + U.Transpose() * u);
+            var l = (X.Transpose() * X + U.Transpose() * U);
+            theta = l.Inverse() * r;
+        }
+    }
+}
---
+++ course/Pages/Home.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace course.Pages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        private DFModel model;
+        private int k1 = 1;
+        private int k2 = 2;
+        private double ro = 0.1;
+
+        public string K1
+        {
+            get { return k1.ToString(); }
+            set
+            {
+                k1 = Convert.ToInt32(value);
+            }
+        }
+        public string K2 {
+            get { return k2.ToString(); }
+            set
+            {
+                k2 = Convert.ToInt32(value);
+            }
+        }
+        public Home()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() != true) return;
+            model.estimate();
+            using (var fout = new StreamWriter(saveFileDialog.FileName))
+            {
+                fout.WriteLine(String.Join(" ", model.tet().Select(v => v.ToString())));
+            }
+
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() != true) return;
+            using (StreamReader fin = new StreamReader(openFileDialog.FileName))
+            {
+                var tet = fin.ReadLine().Split().ToList().ConvertAll<double>(v => Convert.ToDouble(v)).ToArray();
+                int n = Convert.ToInt32(fin.ReadLine());
+                double[][] X = new double[n][];
+                for (int i = 0; i < n; ++i)
+                {
+                    X[i] = fin.ReadLine().Split().ToList().ConvertAll<double>(v => Convert.ToDouble(v)).ToArray();
+                }
+                model = new DFModel(k1, k2, tet, X, ro);
+            }
+            
+        }
+    }
+}
